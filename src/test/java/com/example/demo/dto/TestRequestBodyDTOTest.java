a5876f4d2c641cd427f9e543324cd530e28c2092
IT\cocoa <jws0553@sookmyung.ac.kr>
2023-06-16 11:25:01 +0900

    백엔드 개발 12. @RequestBody 사용하기

---
+++ src/test/java/com/example/demo/dto/TestRequestBodyDTOTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestRequestBodyDTOTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
