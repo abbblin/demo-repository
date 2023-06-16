865238ea5cc23dc1e18efefd680141cd4c48c4ed
IT\cocoa <jws0553@sookmyung.ac.kr>
2023-06-16 22:03:27 +0900

    백엔드 개발 17. @Repository 에서 쿼리 추가하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
-
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+
 
 @Service
 public class TodoService {
@@ -11,12 +11,13 @@ public class TodoService {
 	private TodoRepository repository;
 	public String testService(){
 	// Todo Entity 생성
-	TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+	TodoEntity entity = TodoEntity.builder().userID("user01").title("My first todo item").build();
 	// Todo Entity 저장
 	repository.save(entity);
 	// Todo Entity 검색
-	TodoEntity savedEntity = repository.findById(entity.getId()).get();
-	return savedEntity.getTitle();
+	TodoEntity savedEntity =
+	repository.findByUserID(entity.getUserID()).get(0);
+		return savedEntity.getUserID();
 	}
 
 }
